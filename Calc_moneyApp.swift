1a5fd760c28b4598f7220a662d2fa541c7db6fbb
sanyochek58 <148450042+sanyochek58@users.noreply.github.com>
2024-08-30 19:21:10 +0300

    Add files via upload

---
+++ Calc_moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calc_moneyApp.swift
+//  Calc_money
+//
+//  Created by Александр Бураев on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Calc_moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
